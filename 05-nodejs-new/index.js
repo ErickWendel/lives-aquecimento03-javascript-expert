33d7de583f4c3bc4f3a8521a9df3a16ec8c90f20
Erick Wendel <erick.workspace@gmail.com>
2021-05-15 11:28:45 -0300

    demos in class
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- 05-nodejs-new/index.js
+++ 05-nodejs-new/index.js
@@ -1,1 +1,23 @@
 // Nullish Coalescing
+valor1 = "abc"
+valor2 = "abd"
+valorFinal = valor1 || valor2
+// 'abc'
+valor1 = false
+valorFinal = valor1 || valor2
+// 'abd'
+valorFinal = (0 || false || "") || (null || undefined)
+// 'abd'
+valorFinal =  false ?? true
+// false
+(null || undefined) ?? true
+true
+
+db = {}
+db?.user?.preferences?
+
+db = { user: {}}
+db?.user?.preferences?.["username"].length
+
+db = { user: { preferences: () => ({ result: 1 })}}
+db?.user?.preferences?.().result
--- 05-nodejs-new/timers.mjs
+++
@@ -1,9 +0,0 @@
-import { setTimeout, setInterval } from 'timers/promises';
-
-
-for await (const date of setInterval(1000)) {
-    console.log('running!!')
-}
-
-
-const a = 1
